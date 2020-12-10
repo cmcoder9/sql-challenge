ad8ccdfffb8ae6177c9e1dccf957f499263db105
cmcoder9 <cmcoder@gmail.com>
2020-12-10 11:16:17 -0800

    Q7 works

--- EmployeeSQL/QuickDBD-SQL Challenge EDR.sql
+++ EmployeeSQL/QuickDBD-SQL Challenge EDR.sql
@@ -129,10 +129,15 @@ SELECT first_name, last_name, sex
 FROM Employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
-
 --List all employees in the Sales department, including their employee number, last name, first name, and department name.
-
-
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM Employees AS e
+INNER JOIN Department_Employees AS de
+ON e.emp_no = de.emp_no
+INNER JOIN Departments AS d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales'
+	
 --List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
 
