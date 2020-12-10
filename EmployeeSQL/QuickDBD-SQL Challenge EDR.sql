69612cc1eac80aee1ffde50e103ba9194e873ea4
cmcoder9 <cmcoder@gmail.com>
2020-12-10 11:22:04 -0800

    Q8 works

--- EmployeeSQL/QuickDBD-SQL Challenge EDR.sql
+++ EmployeeSQL/QuickDBD-SQL Challenge EDR.sql
@@ -139,6 +139,12 @@ ON de.dept_no = d.dept_no
 WHERE d.dept_name = 'Sales'
 	
 --List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM Employees AS e
+INNER JOIN Department_Employees AS de
+ON e.emp_no = de.emp_no
+INNER JOIN Departments AS d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development'
 
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
